4f82e619b9c608b53b940ed69f7200025cdac7a6
candymanuu <candymanuu@yahoo.com>
2025-02-22 15:37:34 +0200

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Bogdan"
-pwd
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
@@ -14,12 +12,14 @@ echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
 NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+echo "Bogdan NAME ${NAME}"
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+echo "Bogdan VERSION ${VERSION}"
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
